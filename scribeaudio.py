1902e909b022b846ae177fdf0817ef8158c3f96a
serdarburakyildiz <serdarburakyildiz@outlook.com>
2026-01-29 13:23:04 +0100

    added gitignore and added whisper processing

--- scribeaudio.py
+++ scribeaudio.py
@@ -3,7 +3,7 @@ import os
 
 # Requires ffmpeg to be in PATH 
 
-model whisper.load_model('base')
+model = whisper.load_model('base')
 
 if __name__ == "__main__":
   #Process all audio files in the "audio" folder 
@@ -11,12 +11,24 @@ if __name__ == "__main__":
   if not os.path.exists(audio_folder):
     print(f"Error: Folder '{audio_folder}' not found.")
   else:
-    audio_files [f for f in os.listdir(audio_folder) if f.lower().endswith(('.mp3','.wav','.m4a','.flac','.ogg'))]
+    audio_files = [f for f in os.listdir(audio_folder) if f.lower().endswith(('.mp3','.wav','.m4a','.flac','.ogg'))]
     if not audio_files:
       print(f"No audio files have been found under '{audio_folder}'")
     else:
       for audio_file in audio_files:
         audio_path = os.path.join(audio_folder,audio_file)
         print(f'\nProcessing: {audio_file}')
-        
-  
+        result = model.transcribe(audio_path)
+        output_file = f'{os.path.splitext(audio_file)[0]}_transcript.txt'
+        with open(output_file,'w',encoding='utf-8') as f:
+          f.write("="*80 + "\n")
+          f.write(f'TMESTAMPED SEGMETNS:\n')
+          f.write("="*80 + "\n\n")
+          for segment in result['segments']:
+            start = segment['start']
+            end = segment['end']
+            text = segment['text'].strip()
+            f.write(f"[{start:.2f} --> {end:.2f}] {text}\n")
+          f.write("AUDIO RAW TRANSCRIPT:\n")
+          f.write("="*80 + "\n")
+          f.write(result['text'])
